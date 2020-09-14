9106109db8ba7f7ff27fcca275e12c676aae4ec4
Ziyang Xu <ziyangx@princeton.edu>
2020-09-13 21:18:10 -0400

    create check indirect call

---
+++ run_me.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+export CC=clang
+export CXX=clang++
+
+rm -rf build/ ; 
+mkdir build ; 
+cd build ; 
+cmake -DCMAKE_INSTALL_PREFIX="./install" -DCMAKE_BUILD_TYPE=Debug ../ ; 
+make ;
+make install ;
+cd ../ 
+
+if ! test -e compile_commands.json ; then
+  ln -s build/compile_commands.json ./ ;
+fi
---
+++ src/CatPass.cpp
@@ -0,0 +1,97 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/ADT/Statistic.h"
+
+#include <set>
+
+using namespace llvm;
+STATISTIC(NumICall, "Number of indirect call in the program");
+STATISTIC(NumCoverdByDevirt, "Number of indirect call covered by CPF devirt");
+STATISTIC(NumCoveredByPgo, "Number of indirect call covered by pgo-icall-prom");
+
+namespace {
+
+  struct CAT : public FunctionPass {
+    static char ID; 
+    
+    // static StringRef getFunctionName(CallBase *call)
+    // {
+    //     Function *fun = call->getCalledFunction();
+    //     if (fun) // thanks @Anton Korobeynikov
+    //         return fun->getName(); // inherited from llvm::Value
+    //     else
+    //         return StringRef("indirect call");
+    // }
+
+    CAT() : FunctionPass(ID) {}
+
+    // This function is invoked once at the initialization phase of the compiler
+    // The LLVM IR of functions isn't ready at this point
+    bool doInitialization (Module &M) override {
+      return false;
+    }
+
+    // This function is invoked once per function compiled
+    // The LLVM IR of the input functions is ready and it can be analyzed and/or transformed
+    bool runOnFunction (Function &F) override {
+      // errs() << "Hello LLVM World at \"runOnFunction\"\n" ;
+
+      for (BasicBlock &bb: F){
+        for (Instruction &I: bb){
+          // errs() << "Visiting" << bb << "\n";
+          if (CallBase* cs = dyn_cast<CallBase>(&I)){
+            Function *fn = cs->getCalledFunction();
+            if (!fn) {// indirect call
+              NumICall++;
+              if (!bb.hasName()) // doesn't have a name
+                continue;
+              // if the basic block starts with "default_indirect", then it's covered by CPF's devirt
+              if (bb.getName().startswith("default_indirect")){
+                NumCoveredByDevirt++;
+                continue;
+              }
+
+              // if the basic block starts with "if.false" then it's covered by LLVM pgo-icall-prom
+              if (bb.getName().startswith("if.false")){
+                NumCoveredByPgo++;
+                continue;
+              }
+
+              // else it's a not covered case
+              errs() << I << "\n";
+            }
+
+          }
+        }
+      }
+
+      return true;
+    }
+
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      // AU.setPreservesAll();
+    }
+  };
+}
+
+// Next there is code to register your pass to "opt"
+char CAT::ID = 0;
+static RegisterPass<CAT> X("check-icall", "Check all indirect call",
+    false, // cfg only
+    true // is analysis
+    );
+
+// Next there is code to register your pass to "clang"
+static CAT * _PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
+static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
